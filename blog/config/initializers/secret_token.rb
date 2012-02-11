3d36d33d5957a8ac2ad472ebf8e523510a299feb
Douglas Deodato <douglasdeodato@gmail.com>
2012-02-11 01:28:34 +0000

    como criar um blog com o rails. simples digite no terminal : rails new blog

---
+++ blog/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Blog::Application.config.secret_token = '64d8a1b951364196da5d98fa691feeeb4c119435880e60a90f37e35606f41ad20d748f1f332920a19b8874627d1aec42b08152eca5d79fd143b2f75ddfa75e0e'
