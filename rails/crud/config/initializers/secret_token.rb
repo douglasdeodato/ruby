9f76d15d6fcfb611f15fe2d11a93a21fbf638a20
Douglas Deodato <douglasdeodato@gmail.com>
2012-08-15 12:41:28 +0100

    criando um crud com ruby on rails simples digite o comando no terminal (rails new crud) vai criar toda estrutura.

---
+++ rails/crud/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Crud::Application.config.secret_token = '096c819e5ed56e6f2d2f77b3df98f40035368663d3d4082bb84c318082fbb155c2b4d431158f533b076a0993e3086ed81a5ed1a10429f051e4b11ba19f318baa'
